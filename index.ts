e01996036944b3e120acd4a99d70fb48d243075c
rockhopper-penguin <46835999+rockhopper-penguin@users.noreply.github.com>
2020-08-25 21:34:51 +0900

    サーバ起動の確認

--- index.ts
+++ index.ts
@@ -1,3 +1,14 @@
-import * as QRCode from "qrcode";
+// import QRCode from "qrcode";
 import express from "express";
-QRCode.toFile("test.png", "Rockhopper-Penguin!!!");
+
+const app: express.Express = express();
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  return res.send("Rockhopper-Penguin");
+});
+
+// QRCode.toFile("test.png", "Rockhopper-Penguin!!!");
+
+app.listen(3000);
+console.log(`Server runnning -> http://127.0.0.1:3000`);
+export default app;
