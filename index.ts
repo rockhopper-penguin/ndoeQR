0b3ebed0bb4041271424454ec24ff51df11129f8
rockhopper-penguin <46835999+rockhopper-penguin@users.noreply.github.com>
2020-08-29 18:43:13 +0900

    qrコード作成

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-// import QRCode from "qrcode";
+import QRCode from "qrcode";
 import express from "express";
 
 const app: express.Express = express();
@@ -7,7 +7,7 @@ app.get("/", (req: express.Request, res: express.Response) => {
   return res.send("Rockhopper-Penguin");
 });
 
-// QRCode.toFile("test.png", "Rockhopper-Penguin!!!");
+QRCode.toFile("test.png", "Rockhopper-Penguin!!!");
 
 app.listen(3000);
 console.log(`Server runnning -> http://127.0.0.1:3000`);
