9452f4d75a892feb501c9c4e3cb8882e49181fe4
rockhopper-penguin <46835999+rockhopper-penguin@users.noreply.github.com>
2020-11-01 22:18:44 +0900

    post追加

--- index.ts
+++ index.ts
@@ -1,11 +1,21 @@
 import QRCode from "qrcode";
 import express from "express";
+import bodyParser from 'body-parser';
+
 
 const app: express.Express = express();
+app.use(bodyParser.json());
 
 app.get("/", (req: express.Request, res: express.Response) => {
   return res.send("Rockhopper-Penguin");
 });
+
+let text: string;
+
+app.post("/", (req) => {
+  text = req.body;
+  console.log(`text -> ${JSON.stringify(text)}`);
+})
 
 QRCode.toFile("test.png", "Rockhopper-Penguin!!!");
 
